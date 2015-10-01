e3aa61599d486468bcc558282f99378212e8d4d5
Lee Hicks <leehicks@dreamfactory.com>
2015-10-01 14:29:22 -0400

    set alias usage

--- src/Services/Table.php
+++ src/Services/Table.php
@@ -158,7 +158,7 @@ class Table extends BaseNoSqlDbService
         return $out;
     }
 
-    public function getTableNames($schema = null, $refresh = false)
+    public function getTableNames($schema = null, $refresh = false, $use_alias = false)
     {
         if ($refresh ||
             (empty($this->tableNames) &&
