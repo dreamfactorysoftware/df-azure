96878c366dee53a76c5cd2c61185ab45f63bdc41
Lee Hicks <leehicks@dreamfactory.com>
2016-08-19 15:47:13 -0400

    DF-607 #resolve #comment Making service docs always viewable, even auto-generated ones.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,36 +5,45 @@ use DreamFactory\Core\Azure\Components\AzureBlobConfig;
 use DreamFactory\Core\Azure\Models\AzureConfig;
 use DreamFactory\Core\Azure\Services\Blob;
 use DreamFactory\Core\Azure\Services\Table;
+use DreamFactory\Core\Components\ServiceDocBuilder;
 use DreamFactory\Core\Enums\ServiceTypeGroups;
 use DreamFactory\Core\Services\ServiceManager;
 use DreamFactory\Core\Services\ServiceType;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    use ServiceDocBuilder;
+
     public function register()
     {
         // Add our service types.
-        $this->app->resolving('df.service', function (ServiceManager $df){
+        $this->app->resolving('df.service', function (ServiceManager $df) {
             $df->addType(
                 new ServiceType([
-                    'name'           => 'azure_blob',
-                    'label'          => 'Azure Blob Storage',
-                    'description'    => 'File service supporting the Microsoft Azure Blob storage.',
-                    'group'          => ServiceTypeGroups::FILE,
-                    'config_handler' => AzureBlobConfig::class,
-                    'factory'        => function ($config){
+                    'name'            => 'azure_blob',
+                    'label'           => 'Azure Blob Storage',
+                    'description'     => 'File service supporting the Microsoft Azure Blob storage.',
+                    'group'           => ServiceTypeGroups::FILE,
+                    'config_handler'  => AzureBlobConfig::class,
+                    'default_api_doc' => function ($service) {
+                        return $this->buildServiceDoc($service->id, Blob::getApiDocInfo($service));
+                    },
+                    'factory'         => function ($config) {
                         return new Blob($config);
                     }
                 ])
             );
             $df->addType(
                 new ServiceType([
-                    'name'           => 'azure_table',
-                    'label'          => 'Azure Table Storage',
-                    'description'    => 'Database service supporting the Microsoft Azure Table storage.',
-                    'group'          => ServiceTypeGroups::DATABASE,
-                    'config_handler' => AzureConfig::class,
-                    'factory'        => function ($config){
+                    'name'            => 'azure_table',
+                    'label'           => 'Azure Table Storage',
+                    'description'     => 'Database service supporting the Microsoft Azure Table storage.',
+                    'group'           => ServiceTypeGroups::DATABASE,
+                    'config_handler'  => AzureConfig::class,
+                    'default_api_doc' => function ($service) {
+                        return $this->buildServiceDoc($service->id, Table::getApiDocInfo($service));
+                    },
+                    'factory'         => function ($config) {
                         return new Table($config);
                     }
                 ])
