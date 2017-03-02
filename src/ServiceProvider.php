d7d031bd2efb352e7bd8046cfa997b32ebe7bc6d
Arif Islam <arif@dreamfactory.com>
2017-03-02 15:37:12 -0500

    Minor cleanup

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -58,7 +58,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
                     'group'           => ServiceTypeGroups::DATABASE,
                     'config_handler'  => DocumentDbConfig::class,
                     'default_api_doc' => function ($service) {
-                        return $this->buildServiceDoc($service->id, Table::getApiDocInfo($service));
+                        return $this->buildServiceDoc($service->id, DocumentDB::getApiDocInfo($service));
                     },
                     'factory'         => function ($config) {
                         return new DocumentDB($config);
